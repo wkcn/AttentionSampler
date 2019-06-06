16068951fd8e6f853615e02b3a415dffc0597ebc
wkcn <wkcn@live.cn>
2019-06-06 03:31:49 +0000

    fix bug

--- attention_sampler/attention_sampler.py
+++ attention_sampler/attention_sampler.py
@@ -12,16 +12,16 @@ class AttSampler:
         N, _, in_size, _ = data.shape
         att_size = attx.shape[1]
         out_size = int(in_size * self.scale)
-        threshold = self.scale * self.dense * in_size * att_size
+        threshold = float(self.scale * self.dense * in_size) / att_size
         attx = attx * out_size
         atty = atty * out_size
         for j in range(self.iters):
             max_attx = attx.max(1) # (N, 1)
             max_atty = atty.max(1) # (N, 1)
             if j == 0:
-                threshold = self.F.maximum(self.F.maximum(max_attx, max_atty), threshold) # (N, 1)
+                threshold = self.F.minimum(self.F.minimum(max_attx, max_atty), threshold) # (N, 1)
             else:
-                threshold = self.F.maximum(max_attx, max_atty)
+                threshold = self.F.minimum(max_attx, max_atty)
             self.F.broadcast_minimum(threshold, attx, out=attx)
             self.F.broadcast_minimum(threshold, atty, out=atty)
             sum_x = attx.sum(1) # (N, 1)
@@ -31,19 +31,16 @@ class AttSampler:
             # compensate the drop value
             attx += deltax
             atty += deltay
-        attxi = self.F.empty((N, att_size, 1))
-        attxi[0] = 1
-        attyi = self.F.empty((N, att_size, 1))
-        attyi[0] = 1
+        attxi = self.F.ones((N, att_size, 1))
+        attyi = self.F.ones((N, att_size, 1))
         mobula.func.cumsum(N, attx, attxi, att_size)
         mobula.func.cumsum(N, atty, attyi, att_size)
         stepx = attxi[:, -1] / out_size
         stepy = attyi[:, -1] / out_size
-        myscale = 2.0 / (att_size - 1)
         index_x = self.F.empty((N, out_size, 1))
         index_y = self.F.empty((N, out_size, 1))
-        mobula.func.map_step(N, attxi, index_x, stepx, att_size, out_size)
-        mobula.func.map_step(N, attyi, index_y, stepy, att_size, out_size)
+        mobula.func.map_step(N, attxi, index_y, stepx, att_size, out_size)
+        mobula.func.map_step(N, attyi, index_x, stepy, att_size, out_size)
         grid = self.F.stack(
             index_x.reshape((N, 1, out_size)).tile((1, out_size, 1)),
             index_y.tile((1, 1, out_size)), axis=1)
--- test.py
+++ test.py
@@ -2,11 +2,12 @@ import numpy as np
 import mxnet as mx
 import cv2
 import mobula
+import time
 mobula.op.load('attention_sampler')
 
-map_s = cv2.resize(cv2.imread('imgs/01.jpg'),(224,224))[:,:,0].reshape((1,224,224))
-data = cv2.resize(cv2.imread('imgs/02.jpg'),(224,224))[:,:,0].reshape((1,1,224,224))
-ctx= mx.cpu(0)
+data = cv2.resize(cv2.imread('imgs/01.jpg'),(224,224))[:,:,0].reshape((1,1,224,224))
+map_s = cv2.resize(cv2.imread('imgs/02.jpg'),(224,224))[:,:,0].reshape((1,224,224))
+ctx= mx.gpu(0)
 map_s = mx.nd.array(map_s,ctx=ctx)
 data = mx.nd.array(data,ctx=ctx)
 map_sx = mx.nd.expand_dims(mx.nd.max(map_s, axis=2), axis=-1)
@@ -16,6 +17,25 @@ sum_sy = mx.nd.sum(map_sy, axis=(1,2))
 map_sx = mx.nd.broadcast_div(map_sx, mx.nd.reshape(sum_sx,(0,1,1)))
 map_sy = mx.nd.broadcast_div(map_sy, mx.nd.reshape(sum_sy,(0,1,1)))
 
-data_s = mobula.op.AttSampler(data=data, attx=map_sx, atty=map_sy, scale=0.4375, dense=4)
 mx.nd.waitall()
-print("TEST")
+for _ in range(100):
+    data_pred = mobula.op.AttSampler(data=data, attx=map_sx, atty=map_sy, scale=0.4375, dense=4)
+    data_gt = mx.nd.AttSampler(data=data, attx=map_sx, atty=map_sy, scale=0.4375, dense=4)
+    mx.nd.waitall()
+
+T = 1000
+while 1:
+    tic = time.time()
+    for _ in range(T):
+        data_pred = mobula.op.AttSampler(data=data, attx=map_sx, atty=map_sy, scale=0.4375, dense=4)
+        data_pred.wait_to_read()
+    print("PRED", time.time() - tic)
+
+    tic = time.time()
+    for _ in range(T):
+        data_pred = mx.nd.AttSampler(data=data, attx=map_sx, atty=map_sy, scale=0.4375, dense=4)
+        data_pred.wait_to_read()
+    print("GT", time.time() - tic)
+cv2.imwrite("a.png", data_pred.asnumpy()[0][0])
+
+mobula.testing.assert_almost_equal(data_pred, data_gt)
