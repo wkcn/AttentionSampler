fix broadcast

attention_sampler/attention_sampler.py
@@ -17,16 +17,16 @@ class AttSampler:
         attx = attx * out_size
         atty = atty * out_size
         for j in range(self.iters):
-            max_attx = attx.max(1) # (N, 1)
-            max_atty = atty.max(1) # (N, 1)
+            max_attx = attx.max(1, keepdims=True) # (N, 1, 1)
+            max_atty = atty.max(1, keepdims=True) # (N, 1, 1)
             if j == 0:
-                threshold = self.F.minimum(self.F.minimum(max_attx, max_atty), threshold) # (N, 1)
+                threshold = self.F.minimum(self.F.minimum(max_attx, max_atty), threshold) # (N, 1, 1)
             else:
                 threshold = self.F.minimum(max_attx, max_atty)
             self.F.broadcast_minimum(threshold, attx, out=attx)
             self.F.broadcast_minimum(threshold, atty, out=atty)
-            sum_x = attx.sum(1) # (N, 1)
-            sum_y = atty.sum(1) # (N, 1)
+            sum_x = attx.sum(1, keepdims=True) # (N, 1, 1)
+            sum_y = atty.sum(1, keepdims=True) # (N, 1, 1)
             deltax = (out_size - sum_x) / att_size
             deltay = (out_size - sum_y) / att_size
             # compensate the drop value