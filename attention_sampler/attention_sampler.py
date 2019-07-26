fe5381ceec540eaa08eaa30b39e5f0125de80bd5
wkcn <wkcn@live.cn>
2019-07-26 22:02:31 +0800

    fix out-of-memory

--- attention_sampler/attention_sampler.py
+++ attention_sampler/attention_sampler.py
@@ -1,12 +1,14 @@
 import mobula
 import mxnet as mx
 
+
 @mobula.op.register
 class AttSampler:
     def __init__(self, scale=1.0, dense=4, iters=5):
         self.scale = scale
         self.dense = dense
         self.iters = iters
+
     def forward(self, data, attx, atty):
         # attx: (N, W, 1)
         # atty: (N, H, 1)
@@ -17,16 +19,17 @@ class AttSampler:
         attx = attx * out_size
         atty = atty * out_size
         for j in range(self.iters):
-            max_attx = attx.max(1, keepdims=True) # (N, 1, 1)
-            max_atty = atty.max(1, keepdims=True) # (N, 1, 1)
+            max_attx = attx.max(1, keepdims=True)  # (N, 1, 1)
+            max_atty = atty.max(1, keepdims=True)  # (N, 1, 1)
             if j == 0:
-                threshold = self.F.minimum(self.F.minimum(max_attx, max_atty), threshold) # (N, 1, 1)
+                threshold = self.F.minimum(self.F.minimum(
+                    max_attx, max_atty), threshold)  # (N, 1, 1)
             else:
                 threshold = self.F.minimum(max_attx, max_atty)
             self.F.broadcast_minimum(threshold, attx, out=attx)
             self.F.broadcast_minimum(threshold, atty, out=atty)
-            sum_x = attx.sum(1, keepdims=True) # (N, 1, 1)
-            sum_y = atty.sum(1, keepdims=True) # (N, 1, 1)
+            sum_x = attx.sum(1, keepdims=True)  # (N, 1, 1)
+            sum_y = atty.sum(1, keepdims=True)  # (N, 1, 1)
             deltax = (out_size - sum_x) / att_size
             deltay = (out_size - sum_y) / att_size
             # compensate the drop value
@@ -58,6 +61,7 @@ class AttSampler:
     def backward(self, dy):
         self.out.backward(dy)
         return [self.x.grad, 0, 0]
+
     def infer_shape(self, in_shape):
         dshape = in_shape[0]
         out_size = int(dshape[2] * self.scale)
--- test.py
+++ test.py
@@ -6,24 +6,27 @@ import mobula
 import time
 mobula.op.load('attention_sampler')
 
-data = cv2.resize(cv2.imread('imgs/01.jpg'),(224,224))[:,:,0].reshape((1,1,224,224))
-map_s = cv2.resize(cv2.imread('imgs/02.jpg'),(224,224))[:,:,0].reshape((1,224,224))
+data = cv2.resize(cv2.imread('imgs/01.jpg'), (224, 224)
+                  )[:, :, 0].reshape((1, 1, 224, 224))
+map_s = cv2.resize(cv2.imread('imgs/02.jpg'), (224, 224)
+                   )[:, :, 0].reshape((1, 224, 224))
 ctx = mx.cpu(0)
-map_s = mx.nd.array(map_s,ctx=ctx)
-data = mx.nd.array(data,ctx=ctx)
+map_s = mx.nd.array(map_s, ctx=ctx)
+data = mx.nd.array(data, ctx=ctx)
 map_sx = mx.nd.expand_dims(mx.nd.max(map_s, axis=2), axis=-1)
 map_sy = mx.nd.expand_dims(mx.nd.max(map_s, axis=1), axis=-1)
-sum_sx = mx.nd.sum(map_sx, axis=(1,2))
-sum_sy = mx.nd.sum(map_sy, axis=(1,2))
-map_sx = mx.nd.broadcast_div(map_sx, mx.nd.reshape(sum_sx,(0,1,1)))
-map_sy = mx.nd.broadcast_div(map_sy, mx.nd.reshape(sum_sy,(0,1,1)))
+sum_sx = mx.nd.sum(map_sx, axis=(1, 2))
+sum_sy = mx.nd.sum(map_sy, axis=(1, 2))
+map_sx = mx.nd.broadcast_div(map_sx, mx.nd.reshape(sum_sx, (0, 1, 1)))
+map_sy = mx.nd.broadcast_div(map_sy, mx.nd.reshape(sum_sy, (0, 1, 1)))
 
 mx.nd.waitall()
 
 
 data.attach_grad()
 with mx.autograd.record():
-    data_pred = mobula.op.AttSampler(data=data, attx=map_sx, atty=map_sy, scale=0.4375, dense=4)
+    data_pred = mobula.op.AttSampler(
+        data=data, attx=map_sx, atty=map_sy, scale=0.4375, dense=4)
     data_pred.backward()
 mx.nd.waitall()
 print(data.grad.max(), data.grad.min())
@@ -33,21 +36,25 @@ if not hasattr(mx.nd, 'AttSampler'):
 print("Test consistence")
 
 for _ in range(100):
-    data_pred = mobula.op.AttSampler(data=data, attx=map_sx, atty=map_sy, scale=0.4375, dense=4)
-    data_gt = mx.nd.AttSampler(data=data, attx=map_sx, atty=map_sy, scale=0.4375, dense=4)
+    data_pred = mobula.op.AttSampler(
+        data=data, attx=map_sx, atty=map_sy, scale=0.4375, dense=4)
+    data_gt = mx.nd.AttSampler(
+        data=data, attx=map_sx, atty=map_sy, scale=0.4375, dense=4)
     mx.nd.waitall()
 
 T = 1000
 while 1:
     tic = time.time()
     for _ in range(T):
-        data_pred = mobula.op.AttSampler(data=data, attx=map_sx, atty=map_sy, scale=0.4375, dense=4)
+        data_pred = mobula.op.AttSampler(
+            data=data, attx=map_sx, atty=map_sy, scale=0.4375, dense=4)
         data_pred.wait_to_read()
     print("PRED", time.time() - tic)
 
     tic = time.time()
     for _ in range(T):
-        data_pred = mx.nd.AttSampler(data=data, attx=map_sx, atty=map_sy, scale=0.4375, dense=4)
+        data_pred = mx.nd.AttSampler(
+            data=data, attx=map_sx, atty=map_sy, scale=0.4375, dense=4)
         data_pred.wait_to_read()
     print("GT", time.time() - tic)
 cv2.imwrite("a.png", data_pred.asnumpy()[0][0])
